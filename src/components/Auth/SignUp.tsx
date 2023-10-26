e6bbf8486b0a87dc07d38ad0bd527b5498079030
Richard <richard.h.passos@hotmail.com>
2023-10-26 02:18:15 -0300

    start to create input text

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,38 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const styles = {
-  formControl: {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '20px',
-  },
-  label: {
-    fontSize: '16px',
-    marginBottom: '4px',
-    color: '#3f51b5', // Cor do rótulo
-  },
-  outlinedInput: {
-    border: '1px solid #ccc',
-    padding: '10px',
-    fontSize: '14px',
-    borderRadius: '4px',
-  },
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MaterialTextField = styled.div`
+  position: relative;
+`;
+
+const Label = styled.label`
+  position: absolute;
+  font-size: 1rem;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: white;
+  color: gray;
+  padding: 0 0.3rem;
+  margin: 0 0.5rem;
+  transition: 0.1s ease-out;
+  transform-origin: left top;
+  pointer-events: none;
+`;
+
+const Input = styled.input`
+  font-size: 1rem;
+  outline: none;
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 1rem 0.7rem;
+  color: gray;
+  transition: 0.1s ease-out;
+
+  &:focus {
+    border-color: #6200ee;
+  }
+
+  &:focus + ${Label} {
+    color: #6200ee;
+    top: 0;
+    transform: translateY(-50%) scale(0.9);
+  }
+`;
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <Container>
+      <MaterialTextField>
+        <Input
+          type="text"
+          placeholder=" "
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Label>Email</Label>
+      </MaterialTextField>
+    </Container>
+  );
 };
 
-function MyTextField() {
-  return (
-    <div style={styles.formControl}>
-      <label htmlFor="component-outlined" style={styles.label}>
-        Namae
-      </label>
-      <input
-        type="text"
-        id="component-outlined"
-        style={styles.outlinedInput}
-        defaultValue="Composed TextField"
-      />
-    </div>
-  );
-}
-
-export default MyTextField;
+export default App;
